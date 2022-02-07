b3db8788acfc36faca724898015dced7ae6891b6
Aroosh Kumar <arkumar@cs.washington.edu>
2022-02-06 17:53:47 -0800

    Initial Commit

---
+++ OverlappingObjectsARKit/OverlappingObjectsARKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlappingObjectsARKitApp.swift
+//  OverlappingObjectsARKit
+//
+//  Created by stlp on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct OverlappingObjectsARKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
