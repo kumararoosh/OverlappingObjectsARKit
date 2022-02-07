e29acf5edc1798c86470a03d2c2884edffde8881
Aroosh Kumar <arkumar@cs.washington.edu>
2022-02-06 18:16:53 -0800

    Changed the AR view to show to slightly overlapping objects and two completely overlapping objects

--- OverlappingObjectsARKit/ContentView.swift
+++ OverlappingObjectsARKit/ContentView.swift
@@ -60,6 +60,23 @@ struct RealityKitView: UIViewRepresentable {
             debugPrint("Anchors added to the scene: ", anchors)
             self.focusEntity = FocusEntity(on: view, style: .classic(color: .yellow))
             
+            // places two entities (cubes) in the same location but slightly offset on the x-axis
+            let anchor = AnchorEntity()
+            view.scene.anchors.append(anchor)
+            let box = MeshResource.generateBox(size: 0.5, cornerRadius: 0.05)
+            let redBoxMaterial = SimpleMaterial(color: .red, isMetallic: true)
+            let yellowBoxMaterial = SimpleMaterial(color: .yellow, isMetallic: true)
+            
+            let redBoxEntity = ModelEntity(mesh: box, materials: [redBoxMaterial])
+            let yellowBoxEntity = ModelEntity(mesh: box, materials: [yellowBoxMaterial])
+            
+            redBoxEntity.setPosition([0.2, -1, -2], relativeTo: nil)
+            yellowBoxEntity.setPosition([0, -1, -2], relativeTo: nil)
+            
+            anchor.addChild(redBoxEntity)
+            anchor.addChild(yellowBoxEntity)
+            
+            
         }
         
         @objc func handleTap() {
@@ -69,7 +86,7 @@ struct RealityKitView: UIViewRepresentable {
             let anchor = AnchorEntity()
             view.scene.anchors.append(anchor)
             
-            // Add a Box entity with a blue material
+            // Add a Box entity with alternating blue and green material
             let box = MeshResource.generateBox(size: 0.5, cornerRadius: 0.05)
             var color: UIColor = .blue
             if (count % 2 == 0) {
@@ -79,10 +96,10 @@ struct RealityKitView: UIViewRepresentable {
             }
             count += 1
             
-            
+            // place entities at the same location and observe the difference
             let boxMaterial = SimpleMaterial(color: color, isMetallic: true)
             let diceEntity = ModelEntity(mesh: box, materials: [boxMaterial])
-            diceEntity.setPosition([-0.3 - (0.1 * Float(count)), -1, -2 ], relativeTo: nil)
+            diceEntity.setPosition([-0.5, -1, -2 ], relativeTo: nil)
             
             anchor.addChild(diceEntity)
             
